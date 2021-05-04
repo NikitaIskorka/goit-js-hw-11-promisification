010098ad22779c20729aae06f4f743eaca052cd2
NikitaIskorka <67067414+NikitaIskorka@users.noreply.github.com>
2021-05-04 17:59:55 +0000

    Deploying to gh-pages from @ NikitaIskorka/goit-js-hw-11-promisification@fe0b8f420b308ec02bcf8ca877a68c8a786a0df3 🚀

--- src.f9f7035c.js
+++ src.f9f7035c.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-promisification/src.dc57669f.js.map
+},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"rDzG":[function(require,module,exports) {
+var n=function(n,t){return Math.floor(Math.random()*(t-n+1)+n)},t=function(t){var o=n(200,500);return new Promise(function(n,c){setTimeout(function(){Math.random()>.3&&n({id:t.id,time:o}),c(t.id)},o)})},o=function(n){var t=n.id,o=n.time;console.log("%c Transaction ".concat(t," processed in ").concat(o,"ms"),"color : green")},c=function(n){console.warn(" %c Error processing transaction ".concat(n,". Please try again later."),"color:red")};t({id:70,amount:150}).then(o).catch(c),t({id:71,amount:230}).then(o).catch(c),t({id:72,amount:75}).then(o).catch(c),t({id:73,amount:100}).then(o).catch(c);
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/3");
+},{"./sass/main.scss":"clu1","./js/3":"rDzG"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-11-promisification/src.f9f7035c.js.map
